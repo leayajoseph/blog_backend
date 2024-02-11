98d806254642a1ef429e70dc502977b2fca61e32
leayajoseph <leaya231@gmail.com>
2024-02-11 20:44:01 +0530

    signup completed

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express=require("express")
+const express= require("express")
 const cors=require("cors")
 const mongoose=require("mongoose")
 
+const signupRoute=require("./controller/signupRouter")
 
 const app=express()
 
 app.use(express.json())
 app.use(cors())
 
+mongoose.connect("mongodb+srv://leaya:Leaya231@cluster0.sxtvlhw.mongodb.net/blogDb?retryWrites=true&w=majority",{
+useNewUrlParser: true
+})
 
-mongoose.connect("mongodb+srv://leaya:Leaya231@cluster0.sxtvlhw.mongodb.net/blogDb?retryWrites=true&w=majority",
-{useNewUrlParser:true}
-)
-
+app.use("/api/signup",signupRoute)
 
 app.listen(3001,()=>{
     console.log("server running")
